99347a6fdb464ba8c21266c4d863c87e2995115d
Sreeraj <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-05-17 16:12:59 +0530

    Update ZipRarHunter.py

--- ZipRarHunter/ZipRarHunter.py
+++ ZipRarHunter/ZipRarHunter.py
@@ -174,7 +174,6 @@ def try_rar_password(rar_file, password):
         with rarfile.RarFile(rar_file) as rf:
             rf.setpassword(password)
             test_file = rf.infolist()[0]
-            rf.extract(test_file.filename)
             return True
     except (rarfile.BadRarFile, rarfile.PasswordRequired):
         return False
